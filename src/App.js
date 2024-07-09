d318f8899daf4dced7fcb333d7f9a7c2fb6b6a81
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-07-09 16:13:12 -0300

    a

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './style.css'
+import React, { useEffect, useState, useTransition } from 'react';
+
+export default function App() {
+
+  return (
+    <section>
+      <h1>oi</h1>
+    </section>
+  )
+}
